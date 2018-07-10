1ad49c00d1ecda13126effb522735c5f3297833f
jie <xxjs_01@zonewin.net.cn>
2018-07-10 22:33:33 +0800

    add

---
+++ StudyPlayground.playground/Pages/常见注释.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+// 单行注释
+//var a = 0
+
+
+// 文案注释
+/// 姓名
+var name = "123"
+
+// 文档注释
+/// 测试函数
+///
+/// - Parameter urls: url
+/// - Returns: 地址数
+func test(urls: String) -> Int {
+    return 0
+}
+
+// 分组
+// MARK: -abc
+
+
+// TODO: - 需要接着做的
+
+// FIXME: 检查
+
--- StudyPlayground.playground/Pages/懒加载.xcplaygroundpage/Contents.swift
+++ StudyPlayground.playground/Pages/懒加载.xcplaygroundpage/Contents.swift
@@ -6,3 +6,14 @@ import Foundation
 // 1. 使用的时候才加载，节省资源
 // 2. 可以防止重复创建
 // 3. 不用关心代码的顺序
+
+// lazy修饰为懒加载
+// 懒加载 = 后面可以跟值、具体的变量、构造函数
+// 所谓的懒加载是指在用到的时候在通过后面的函数获取相应的实例
+class Person {
+    lazy var name: String = "xxx"
+    
+}
+
+let p = Person()
+p.name
